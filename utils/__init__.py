e029abb214f30d897712bb3f7e0870f4957dccef
Jan Koloda <jkoloda@das-nano.com>
2018-09-27 15:05:30 +0200

    Add image transformation tools

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,9 @@
 """Image (pre)processing utilities."""
 
 import numpy as np
-import cv2
 import random
+import math
+from scipy.interpolate import griddata
 
 
 def extract_random_block(img, blk_size):
@@ -89,3 +90,90 @@ def add_random_sign(x):
     """
     sign = random.choice([+1, -1])
     return sign * x
+
+
+def build_transformation_matrix(rotation=0, zoom=0, translation=(0, 0)):
+    """Build transformation matrix according to specifications.
+
+    Parameters
+    ----------
+    rotation : int
+        Clockwise rotation agnle in degrees.
+
+    zoom : float
+        Zoom ratio. A value of 0.15 means a zoom-in of 15%. A value of -0.15
+        means a zoom-out of 15%.
+
+    translation : tuple
+        Tuple of (x,y) values that are to be used for translation.
+
+    Returns
+    -------
+    T : ndarray
+        A 2x2 transformation matrix.
+
+    t : ndarray
+        A 2x1 translation vector.
+
+    """
+    angle = math.radians(rotation)
+    R = np.array([[np.cos(angle), -np.sin(angle)],
+                  [np.sin(angle), np.cos(angle)]])
+
+    Z = np.array([[1+zoom, 0], [0, 1+zoom]])
+    t = np.reshape(np.asarray(translation), (2, 1))
+    return np.matmul(R, Z), t
+
+
+def transform_image(img, T, t):
+    """Apply affine trnaform to image.
+
+    Parameters
+    ----------
+    img : ndarray
+        Image that is to be transformed.
+
+    T : ndarray
+        A 2x2 transformation matrix.
+
+    t : ndarray
+        A 2x1 translation vector.
+
+    Returns
+    -------
+    trans_img : ndarray
+        Transformed image.
+
+    mesh_r : ndarray
+        Rows of the resulting floating mesh.
+
+    mesh_c : ndarray
+        Columns of the resulting floating mesh.
+
+    """
+    [rows, cols] = img.shape
+
+    # Designed for even dimensions only
+    assert (rows % 2 == 0 and rows % 2 == 0)
+
+    # Compute floating mesh
+    reference = [rows/2+0.5, cols/2+0.5]
+    mesh_r = np.zeros((rows, cols))
+    mesh_c = np.zeros((rows, cols))
+    for r in range(0, rows):
+        for c in range(0, cols):
+            position = np.reshape(
+                        np.array((r - reference[1], c - reference[0])), (2, 1))
+            temp = np.matmul(T, position) + t
+            mesh_r[r, c] = temp[1] + reference[0]
+            mesh_c[r, c] = temp[0] + reference[1]
+
+    # Estimate transformed image
+    grid_x, grid_y = np.mgrid[0:cols:1, 0:rows:1]
+    points = np.transpose(np.array([mesh_r.ravel(), mesh_c.ravel()]))
+    points = points.astype(np.float32)
+    xi = (grid_x, grid_y)
+    out = griddata(points, (img.ravel()).astype(np.float32),
+                   xi, method='linear')
+
+    return out, mesh_r, mesh_c
